67b112daaf7ccee054130f811158f0e5041ffab8
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2024-04-08 20:22:18 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -263,8 +263,8 @@ def hyperP_scaler(corpus):
     INPUT: a dataset of text (a corpus)
     OUTPUT: min_topic_size and n_neighbors (for UMAP) hyperparameter choices
     '''
-    topic_size = int(len(corpus) / 2000 * 10) + 1
-    umap_size = int(len(corpus) / 2000 * 15) + 1
+    topic_size = int(len(corpus) / 2000 * 10) + 2
+    umap_size = int(len(corpus) / 2000 * 15) + 2
     return topic_size, umap_size
 
 
